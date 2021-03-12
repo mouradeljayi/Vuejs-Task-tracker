64aba1090ae1a7aa3c08aab851810ca3794b0b0b
Mourad EL Jayi <mouradeljayi98@gmail.com>
2021-03-12 18:02:56 +0100

    building before production

---
+++ vue-task-tracker/src/components/Button.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <button @click="onClick()" :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      this.$emit('btn-click')
+    }
+  }
+}
+</script>
---
+++ vue-task-tracker/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <header>
+    <h1>{{ title }}</h1>
+    <Button @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? '#ac0d0d' : '#046582'" />
+  </header>
+</template>
+
+<script>
+import Button from './Button'
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    showAddTask: Boolean
+  },
+  components: {
+    Button
+  }
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
